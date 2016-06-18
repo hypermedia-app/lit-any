412759c3e240d9bac419d88e67007f24e14777ae
tpluscode <tpluscode@users.noreply.github.com>
2016-06-18 16:04:26 +0200

    object template

--- src/augeas.ts
+++ src/augeas.ts
@@ -1,2 +1,3 @@
 import './templates/ags-array-template'
 import './templates/ags-literal-template'
+import './templates/ags-object-template'
---
+++ src/templates/ags-object-template.ts
@@ -0,0 +1,14 @@
+import {RegisteredTemplate} from "../template-registry";
+
+@behavior(RegisteredTemplate)
+@extend('template')
+@component('ags-object-template')
+class AnyObjectTemplate extends polymer.Base {
+    objectMatches(resource) {
+        var isObject = typeof resource === 'object' && !!resource['@id'];
+
+        return isObject;
+    }
+}
+
+AnyObjectTemplate.register();
