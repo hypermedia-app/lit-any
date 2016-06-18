e8a6abbcd390f8e5eb9fce6902cc52ba72347889
tpluscode <tpluscode@users.noreply.github.com>
2016-06-18 16:47:51 +0200

    type template

--- src/augeas.ts
+++ src/augeas.ts
@@ -1,3 +1,4 @@
 import './templates/ags-array-template'
 import './templates/ags-literal-template'
 import './templates/ags-object-template'
+import './templates/ags-type-template'
---
+++ src/templates/ags-type-template.ts
@@ -0,0 +1,25 @@
+import {RegisteredTemplate} from '../template-registry';
+
+@component('ags-type-template')
+@behavior(RegisteredTemplate)
+@extend('template')
+class TypeTemplate extends polymer.Base {
+
+    @property()
+    type: String;
+
+    objectMatches(res) {
+        if(!this.type) {
+            console.warn('type property is not set for ags-type-template');
+            return false;
+        }
+
+        if(Array.isArray(res['@type'])) {
+            return res['@type'].some(t => t === this.type);
+        }
+
+        return res['@type'] === this.type;
+    }
+}
+
+TypeTemplate.register();
