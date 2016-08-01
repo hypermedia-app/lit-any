cc2224dbce190d47c01fab76188ac385ddb23ab4
tpluscode <tpluscode@users.noreply.github.com>
2016-08-01 20:32:38 +0200

    use test-fixture fixture function

--- test/_TestHelpers.js
+++ test/_TestHelpers.js
@@ -1,18 +1,3 @@
-function prepareFixture(fixtureId) {
-    var fixture;
-
-    beforeEach(function () {
-        fixture = document.querySelector('test-fixture#' + fixtureId);
-        fixture.create();
-    });
-
-    afterEach(function () {
-        if (fixture && fixture.restore) {
-            fixture.restore();
-        }
-    });
-}
-
 function setInputValue(input, text) {
     var e = document.createEvent('TextEvent');
     e.initTextEvent(
