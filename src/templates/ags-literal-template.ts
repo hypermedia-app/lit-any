a01f21069cebf1bca97fd93fa917a16be0f3ecb6
tpluscode <tpluscode@users.noreply.github.com>
2017-01-25 22:24:02 +0100

    pass tests of the rest of templates

--- src/templates/ags-literal-template.ts
+++ src/templates/ags-literal-template.ts
@@ -2,7 +2,7 @@ import "link!../behaviors/AgsViewTemplate.html";
 import {behavior} from "twc/annotations/polymer";
 
 @behavior(Augeas.AgsViewTemplate)
-export class AnyLiteralTemplate {
+export class AgsLiteralTemplate {
     objectMatches(resource) {
         return !(typeof resource === 'object') || !! resource['@value'];
     }
