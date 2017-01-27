2d86805d6497912039ff5c0b1f4accb38064e2c3
tpluscode <tpluscode@users.noreply.github.com>
2017-01-27 07:45:28 +0100

    move compactWith

--- dist/templates/ags-object-template.d.ts
+++ dist/templates/ags-object-template.d.ts
@@ -1,4 +1,12 @@
 import "link!../behaviors/AgsViewTemplate.html";
-export declare class AnyObjectTemplate {
+export declare class AgsObjectTemplate {
+    /**
+     * If truthy, it will be used as `@context` in JSON-LD API to compact the object before
+     * binding with the rendered template
+     */
+    compactWith: Object;
+    /**
+     * Returns true if the value is a JS object and contains and `@id`
+     */
     objectMatches(resource: any): boolean;
 }
--- src/templates/ags-object-template.ts
+++ src/templates/ags-object-template.ts
@@ -3,6 +3,16 @@ import {behavior} from "twc/annotations/polymer";
 
 @behavior(Augeas.AgsViewTemplate)
 export class AgsObjectTemplate {
+
+    /**
+     * If truthy, it will be used as `@context` in JSON-LD API to compact the object before
+     * binding with the rendered template
+     */
+    compactWith: Object;
+
+    /**
+     * Returns true if the value is a JS object and contains and `@id`
+     */
     objectMatches(resource) {
         return typeof resource === 'object' && !!resource['@id'];
     }
