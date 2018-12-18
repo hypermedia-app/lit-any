b64932207681ce07b2adc91f4033c549adba7c3e
tpluscode <tpluscode@users.noreply.github.com>
2018-12-18 13:46:51 +0100

    tweak bs config

--- test/karma.es5.bs.config.js
+++ test/karma.es5.bs.config.js
@@ -1,16 +1,31 @@
-const localConfig = require('./karma.conf');
 const createBaseConfig = require('@open-wc/testing-karma-bs/create-karma-es5-bs.config');
 
 module.exports = (config) => {
-    const baseConfig = localConfig(config);
-    const bsConfig = createBaseConfig(baseConfig);
+    const baseConfig = createBaseConfig(config);
 
     config.set({
-        ...bsConfig,
+        ...baseConfig,
+
+        files: [
+            // if --grep flag is set, run those tests instead
+            config.grep ? config.grep : '**/*.test.js',
+        ],
+
+        webpack: {
+            ...baseConfig.webpack,
+        },
 
         browserStack: {
-            ...bsConfig.browserStack,
+            ...baseConfig.browserStack,
             project: 'lit-any',
         },
     });
+
+
+    const babelLoader = config.webpack.module.rules.find(rule => rule.loader === 'babel-loader');
+    babelLoader.options.plugins = babelLoader.options.plugins || [];
+    babelLoader.options.plugins.push('@babel/plugin-syntax-dynamic-import');
+    babelLoader.options.plugins.push([
+        '@babel/plugin-transform-regenerator',
+    ]);
 };
